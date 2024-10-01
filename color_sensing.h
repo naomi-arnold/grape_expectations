b0fb09b60fabddce2fe8dba7bf51fa43cfac7856
daniprice <152434162+daniprice@users.noreply.github.com>
2024-10-01 13:12:14 -0400

    Add files via upload
    initial files for color sensing

---
+++ color_sensing.h
@@ -0,0 +1,19 @@
+#ifndef COLOR_SENSING_H
+#define COLOR_SENSING_H
+
+class Color_sensing {
+  public:
+    Color_sensing();
+    void color_sensing_setup();
+    void determine_color();
+
+  private:
+    // analog ports where voltage from color reflections is being read from
+    int blue_signal = A0;
+    int red_signal = A1;
+
+    // values voltages from color reflections
+    int blue_value = 0;
+    int red_value = 0;
+};
+#endif
